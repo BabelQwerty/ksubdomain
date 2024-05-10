26e2d303d3324f32d323e3c8aa7887fd5e9743ef
BabelQwerty <107346740+BabelQwerty@users.noreply.github.com>
2024-05-11 00:27:18 +0800

    Update banner.go

--- core/banner.go
+++ core/banner.go
@@ -5,14 +5,7 @@ import (
 	"github.com/boy-hack/ksubdomain/core/gologger"
 )
 
-const banner = `
- _              _         _                       _       
-| | _____ _   _| |__   __| | ___  _ __ ___   __ _(_)_ __  
-| |/ / __| | | | '_ \ / _' |/ _ \| '_ ' _ \ / _| | | '_ \
-|   <\__ \ |_| | |_) | (_| | (_) | | | | | | (_| | | | | |
-|_|\_\___/\__,_|_.__/ \__,_|\___/|_| |_| |_|\__,_|_|_| |_|
-
-`
+const banner = ``
 
 func ShowBanner() {
 	gologger.Printf(banner)
